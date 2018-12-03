cc739bb9a73ae4bd49240f25d8ac4c52479d495a
donald24ever <donaldo24ever@gmail.com>
2018-12-03 15:00:13 +0100

    weather-App

---
+++ components/SearchInput.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import { StyleSheet, TextInput, View} from 'react-native';
+import PropTypes from 'prop-types';
+
+export default class SearchInput extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+          text: '',
+        };
+    }
+//using property initalizers which allows us to declear the member methods as arrow functions
+    handleChangeText = text => {
+        //doing something about the new location
+        this.setState({text});
+    }
+    handleSubmitEditing = () => {
+        const { onSubmit } = this.props;
+        const {text}  = this.state;
+
+        if(!text) return;
+
+        onSubmit(text);
+        this.setState({text: ''});
+    };
+    render() {
+        const {placeholder} = this.props;
+        const {text} = this.state;
+        return (
+         <View style={styles.container}>
+          <TextInput
+            autoCorrect={false}
+            value={text}
+            placeholder={placeholder}
+            placeholderTextColor="white"
+            underlineColorAndroid="transparent"
+            style={styles.textInput}
+            clearButtonMode="always"
+            onChangeText={this.handleChangeText}//This method with a function passed is invoked everytime the test within the input is changed.
+            onSubmitEditing={this.handleSubmitEditing}
+        />
+         </View>
+        );
+    }
+}
+
+SearchInput.propTypes = {
+    onSubmit: PropTypes.func.isRequired,
+    placeholder: PropTypes.string,
+  };
+  
+  SearchInput.defaultProps = {
+    placeholder: '',
+  };
+  
+const styles = StyleSheet.create({
+    container: {
+       backgroundColor: '#666',
+       height: 40,
+       width: 300,
+       marginTop: 20,
+       marginHorizontal: 20,
+       paddingHorizontal: 10,
+       borderRadius: 5,
+    },
+    textInput: {
+        flex: 1,
+        color: 'white',
+    }
+});
